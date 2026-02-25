680358d54587d99dd27efd4dc1104e04e8b413c3
masilamanip99 <masilamanip99@gmail.com>
2026-02-25 17:11:48 +0530

    Update Problem5.js

--- Problem5.js
+++ Problem5.js
@@ -1,3 +1,8 @@
+// Problem 5 – Performance Optimization 
+// Explain database indexing, caching strategy, and cache invalidation for optimizing partner 
+// availability APIs. 
+
+
 DATABASE INDEXING :
 
  Database indexing improves query speed by avoiding full table scans.
@@ -8,5 +13,6 @@ CACHING :
  Caching using Redis reduces database load by storing frequently accessed availability results. Cache keys can be city-based (e.g., availability:Mumbai).
 
 CAche Invalidation :
+
 
  Cache invalidation must happen when booking status changes or partner availability changes. This can be done either manually (deleting cache on updates) or using short TTL
